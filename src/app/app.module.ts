3326bbb53ec4df608e6e8bf0947026787698b5f6
devseme <ian.ochenge@student.moringaschool.com>
2021-10-10 02:25:02 +0300

    working on the child component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { QuotesComponent } from './quotes/quotes.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
-    QuotesComponent
+    QuotesComponent,
+    QuoteDetailsComponent
   ],
   imports: [
     BrowserModule,
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -8,7 +8,7 @@ import {Quote} from '../quote';
 export class QuotesComponent implements OnInit {
 
   quotes:Quote[]=[
-    new Quote('Quote 1','Believe you can and you are halfway there.','-Theodore Roosevelt','5'),
+    new Quote('Quote 1','Believe you can, and you are halfway there!','-Theodore Roosevelt','5'),
     new Quote('Quote 2','The only person you are destined to become is the person you decide to be.','-Ralph Waldo Emerson','4'),
     new Quote('Quote 3','Nothing is impossible, the word itself says, ‘I am possible!' ,'-Audrey Hepburn','3'),
   ]
